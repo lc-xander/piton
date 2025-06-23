38672285a3712bb3fc7700def9f2f050a05db96b
lc-xander <alexblox13@gmail.com>
2025-06-22 19:21:45 -0600

    primer commit del generador de contraseñas

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+print("generador de contraseñas aleatorias")
+caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+longitud = int(input("Ingrese la longitud de la contraseña: "))
+contraseña = "".join(random.choice(caracteres) for _ in range(longitud))
+print("contraseña generada")
+print(contraseña)
